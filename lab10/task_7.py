c13b8195581dbbd9815e161b9d9d6b9a82d40310
Sergey <yakushevskiy.s.s@mail.ru>
2024-10-29 13:27:14 +0300

    add comments task_7

--- lab10/task_7.py
+++ lab10/task_7.py
@@ -1,28 +1,26 @@
-import math
-
 import numpy as np
 from matplotlib import pyplot as pl
 from scipy.fft import fft
 
-dt = 0.0001
-t = np.arange(0, 1, dt)
+dt = 0.0001 # задаём шаг деления отрезка
+t = np.arange(0, 1, dt) # вычисляем значения на отрезке от 0 до 1 с шагом t
 y = (2 * np.sin(2 * np.pi * t) + 3 * np.cos(2 * np.pi * 3 * t) +
-     0.5 * np.sin(2 * np.pi * 10 * t) + np.random.randn(len(t)))
+     0.5 * np.sin(2 * np.pi * 10 * t) + np.random.randn(len(t))) # задаём гармоническую функцию
 
-N = len(t)
-Y = fft(y)
+N = len(t) # вычисляем количество точек
+Y = fft(y) # быстрое преобразование фурье
 
-A = abs(2*Y/N)
-fs = 1/dt
+A = abs(2*Y/N) # амплитуда составляющей спектра
+fs = 1/dt # дисккрета по частоте
 m = np.array(range(N))
-f = m * fs / N
+f = m * fs / N # составляющая спектра
 
-pl.subplot(1, 3, 1)
+pl.subplot(1, 3, 1) # построение спектра
 pl.plot(t, y, 'k')
 pl.xlabel('t')
 pl.ylabel('y(t)')
 
-pl.subplot(1, 3, 2)
+pl.subplot(1, 3, 2) # график дискретного амплитудно-частотного спектра
 pl.plot(f, A, 'k')
 pl.xlabel('f')
 pl.ylabel('A(f)')
